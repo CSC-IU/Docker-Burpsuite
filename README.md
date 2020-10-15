5023fd47925b89dab7844cf1e673594f12519fc3
Alex <alexyao2015@gmail.com>
2020-10-15 17:14:02 -0400

    heya

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-Labs
+#Labs
+Heya you found the burpsuite branch
+```bash
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock downloader.tar
+```
---
+++ dockerdownloader/entry.sh
@@ -0,0 +1,15 @@
+!#/bin/sh
+
+echo "Beginning File Download"
+ggID='10kVVvs5R8IVOr2krS1nW2D4o3Tddvxkn'  
+ggURL='https://drive.google.com/uc?export=download'  
+filename="$(curl -sc /tmp/gcokie "${ggURL}&id=${ggID}" | grep -o '="uc-name.*</span>' | sed 's/.*">//;s/<.a> .*//')"  
+getcode="$(awk '/_warning_/ {print $NF}' /tmp/gcokie)"  
+curl -Lb /tmp/gcokie "${ggURL}&confirm=${getcode}&id=${ggID}" -o "${filename}"
+rm -rf /tmp/gcokie
+
+echo "File downloaded. Loading docker image"
+docker import /tmp/burp.tar yaoa/testimage
+echo "Image loaded. Starting image..."
+docker run -d -e PUID=1000 -e PGID=1000 -p 3000:3000 yaoa/testimage
+echo "Complete! Exiting..."
